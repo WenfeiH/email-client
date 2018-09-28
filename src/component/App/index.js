40940dbe8d179ea8b4844bfe10b28d513a0ba3ea
Wenfei <wenfeihan1@gmail.com>
2018-09-28 16:44:22 -0700

    fix the bug that when delete the last email in folder will get err

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -45,14 +45,40 @@ class App extends Component {
   deleteEmail = id => {
     let arr = this.state.emails;
     let index = arr.findIndex(item => item.id === id);
-
     let ss = arr.findIndex(item => item.id > id && item.tag === arr[index].tag);
+
+    //这里我要当要删除的email是folder中的最后一个时候（不再有id比这条大的了，ss是-1），删除之后显示倒数它前一条email，也就是id比它小的最后一条
+    //很笨拙，先把email arr反转，然后找到反转后的第一个满足条件的，最后通过长度减去这个index得到在原来arr里面的位置
+    //有什么好方法么
+    //并且还有个问题，将arr reverse之后还要reverse回来，this.state.email里面顺序才不会改变
+    if (ss === -1) {
+      // console.log(this.state.emails);
+      // console.log(arr);
+      ss = arr
+        .reverse()
+        .findIndex(item => item.id < id && item.tag === arr[index].tag); //为什么这里reverse了arr会影响到原来的email顺序呢，还需要reverse回来才行，不是只能通过setState修改么
+      // console.log(this.state.emails);
+      // console.log(arr);
+      arr.reverse();
+      ss = arr.length - 1 - ss;
+    }
+    console.log("read", ss);
     this.readEmail(ss);
+
     //这里我想要在得到删除一条email之后它的下一条email的id，然后call readEmail去读取下一条的信息
     //我实现了，但是我不太清楚期间的运行关系
     //在readEmail后有state更新，那么便会立刻重新render，那接下来这两句也照样执行？然后再render？看结果是这样的
-    arr[index].tag = "deleted";
-    this.setState({ emails: arr });
+    // arr[index].tag = "deleted";
+    this.setState({
+      emails: [
+        ...this.state.emails.slice(0, index),
+        {
+          ...this.state.emails[index],
+          tag: "deleted"
+        },
+        ...this.state.emails.slice(index + 1)
+      ]
+    });
   };
 
   render() {
